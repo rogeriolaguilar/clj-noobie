0d07c4aa9c89d78bea9847b23616925ae49efaaa
Rogerio Libarino Aguilar <rogerio.libarino@nubank.com.br>
2021-10-10 17:04:29 -0300

    Require modules. Use group-by. Perform complex transformation

---
+++ src/clj_noobie/alura3.clj
@@ -0,0 +1,73 @@
+(ns clj_noobie.alura3
+  (:require [clj_noobie.db :as n.db]))
+
+(println (n.db/all-orders))
+
+; grouping order by user
+(group-by :user (n.db/all-orders))
+
+(defn count-orders-by-user
+ [[user orders]]
+ {:user-id user
+  :orders-count  (count orders)})
+
+; count order by user
+(map count-orders-by-user
+     (group-by :user
+               (n.db/all-orders)))
+; or
+(->> (n.db/all-orders)
+     (group-by :user)
+     (map count-orders-by-user))
+
+
+
+(defn total-price-per-item
+  [item]
+  (* (get item :qtd 0) (get item :unit-price 0)))
+
+; test
+(total-price-per-item {:id :mochila, :qtd 2, :unit-price 80})
+(total-price-per-item {:id :tenis, :qtd 1})
+
+
+(defn total-price-per-order
+  [order]
+  (->>
+    (vals order)
+    (map total-price-per-item)
+    (reduce +)))
+
+; test
+(total-price-per-order {:mochila  {:id :mochila, :qtd 2, :unit-price 80},
+                        :camiseta {:id :camiseta, :qtd 3, :unit-price 40},
+                        :tenis    {:id :tenis, :qtd 1}})
+
+(defn total-price
+  [orders]
+  (->> orders
+       (map :items)
+       (map total-price-per-order)
+       (reduce +)))
+
+; test
+(total-price [{:user 15,
+               :items {:mochila {:id :mochila, :qtd 2, :unit-price 80},
+                       :camiseta {:id :camiseta, :qtd 3, :unit-price 40},
+                       :tenis {:id :tenis, :qtd 1}}}
+              {:user 15,
+               :items {:mochila {:id :mochila, :qtd 3, :unit-price 80},
+                       :camiseta {:id :camiseta, :qtd 3, :unit-price 40},
+                       :tenis {:id :tenis, :qtd 1}}}])
+
+
+(defn orders-and-total-price-by-user
+  [[user orders]]
+  {:user-id user
+  :orders-count (count orders)
+  :total-price (total-price orders)})
+
+; test
+(->> (n.db/all-orders)
+     (group-by :user)
+     (map orders-and-total-price-by-user))
---
+++ src/clj_noobie/db.clj
@@ -0,0 +1,30 @@
+(ns clj_noobie.db)
+
+(def order1 {:user 15
+            :items { :mochila { :id :mochila :qtd 2 :unit-price 80}
+                     :camiseta { :id :camiseta :qtd 3 :unit-price 40}
+                    :tenis { :id :tenis :qtd 1}}})
+
+(def order2 {:user 1
+             :items { :mochila { :id :mochila :qtd 22 :unit-price 80}
+                     :camiseta { :id :camiseta :qtd 3 :unit-price 40}
+                     :tenis { :id :tenis :qtd 1}}})
+
+(def order3 {:user 15
+             :items { :mochila { :id :mochila :qtd 3 :unit-price 80}
+                     :camiseta { :id :camiseta :qtd 3 :unit-price 40}
+                     :tenis { :id :tenis :qtd 1}}})
+
+(def order4 {:user 2
+             :items { :mochila { :id :mochila :qtd 7 :unit-price 60}
+                     :camiseta { :id :camiseta :qtd 3 :unit-price 40}
+                     :tenis { :id :tenis :qtd 1}}})
+
+(def order5 {:user 3
+             :items { :mochila { :id :mochila :qtd 2 :unit-price 80}
+                     :camiseta { :id :camiseta :qtd 3 :unit-price 40}
+                     :tenis { :id :tenis :qtd 1}}})
+
+
+(defn all-orders []
+  [order1 order2 order3 order4 order5])
