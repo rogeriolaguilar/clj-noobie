5fef134d4b8716faeadd515de6982ac282d4ab7e
Rogerio Libarino Aguilar <rogerio.libarino@nubank.com.br>
2021-10-13 16:22:53 -0300

    Extract common test variables

--- test/clj_noobie/tests/logic_test.clj
+++ test/clj_noobie/tests/logic_test.clj
@@ -16,25 +16,26 @@
   (testing "when the department do not exists"
     (is (not (queue-has-space? {:waiting-room [1 2 3]} :other-department)))))
 
-(deftest check-in_test
-  (testing "check-in if there is space"
-    (is (= {:waiting-room [1 2 3 4]}
-           (check-in {:waiting-room [1 2 3]} :waiting-room 4))))
+  (let [full-waiting-room {:waiting-room [1 2 3 4 5]}
+        not-full-waiting-room {:waiting-room [1 2 3]}]
+    (deftest check-in_test
+      (testing "check-in if there is space"
+        (is (= {:waiting-room [1 2 3 4]}
+               (check-in not-full-waiting-room :waiting-room 4))))
 
-  (testing "check-in when department do not exists"
-    (is (= {:waiting-room [1 2 3]}
-           (check-in {:waiting-room [1 2 3]} :not-existent-key :foo))))
+      (testing "check-in when department do not exists"
+        (is (= not-full-waiting-room
+               (check-in not-full-waiting-room :not-existent-key :foo))))
 
-  (testing "check-in when the department has not space"
-    (is (thrown-with-msg? ExceptionInfo #"not space"
-                          (check-in {:waiting-room [1 2 3 4 5]} :waiting-room 6))))
+      (testing "check-in when the department has not space"
+        (is (thrown-with-msg? ExceptionInfo #"not space"
+                              (check-in full-waiting-room :waiting-room 6))))
 
-  (testing "check-in thrown error with info when the department has not space"
-    (is (try
-              (check-in {:waiting-room [1 2 3 4 5]} :waiting-room 6)
+      (testing "check-in thrown error with info when the department has not space"
+        (is (try
+              (check-in full-waiting-room :waiting-room 6)
               false
-            (catch ExceptionInfo e
-              (= {:person 6 :department :waiting-room} (ex-data e))
-             )))))
+              (catch ExceptionInfo e
+                (= {:person 6 :department :waiting-room} (ex-data e))))))))
 
 
