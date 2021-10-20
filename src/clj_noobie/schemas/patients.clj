b66cf2592aa6a3293cb50c2ae4a3c3001b0d0aa5
Rogerio Libarino Aguilar <rogerio.libarino@nubank.com.br>
2021-10-20 19:04:57 -0300

    Schemas: Define schemas with custom and composite schemas

--- src/clj_noobie/schemas/patients.clj
+++ src/clj_noobie/schemas/patients.clj
@@ -39,8 +39,66 @@
 (s/defn new-patient
   [id :- Long
    name :- s/Str]
-  {:id id
+  {:id   id
    :name name})
 
 (pprint (new-patient 15 "AAA"))
 ;(pprint (new-patient "33" "AAA")); raise error
+
+
+(println "Defining scheme for Patients")
+(def Patient
+  "Schema of Patients"
+  {:id s/Num :name s/Str})
+
+(println "testing the schema")
+(pprint (s/validate Patient {:id 15 :name "RRR"}))
+; (pprint (s/validate Patient {:id 15})) ; raise error
+; (pprint (s/validate Patient {:id 15 :name "RRR" :new-key "AAA"})) ; raise error
+
+(println "using the Patient schema to define a function return")
+(s/defn new-patient :- Patient
+  [id :- s/Num, name :- s/Str]
+  {:id id :name name})
+
+
+(println "Define schema with some logic (using s/pred)")
+(s/defn positive-number? [number] (> number 0))
+(def PositiveNumber (s/pred positive-number?))
+
+; (pprint (s/validate PositiveNumber -1)) raise error
+(pprint (s/validate PositiveNumber 43))
+
+(println "Defining scheme for Patients again, but composing schema validations")
+(def Patient
+  "Schema of Patients"
+  {:id (s/constrained s/Int pos?) :name s/Str})
+
+(pprint (s/validate Patient {:id 42 :name "RRR"}))
+; (pprint (s/validate Patient {:id -42 :name "RRR"}))         ; raise error
+
+;; Do not use lambdas in schemas!!! The error when using lambdas are not properly logged
+
+(println "Define schema for vectors")
+(def Numbers [PositiveNumber])
+(pprint (s/validate Numbers [1 2 3 4]))
+(pprint (s/validate Numbers []))
+(pprint (s/validate Numbers nil))
+;(pprint (s/validate Numbers [1 2 3 -4]))                    ; raise error
+
+
+(println "Define schema with optional keys (By default, keys are required in Schema)")
+(def Patient
+  "Schema of Patients"
+  {:id                   (s/constrained s/Int pos?)
+   :name                 s/Str
+   (s/optional-key :age) s/Int})
+(pprint (s/validate Patient {:id 42 :name "RRR"}))
+(pprint (s/validate Patient {:id 42 :name "RRR" :age 21}))
+;(pprint (s/validate Patient {:id 42 :name "RRR" :age 21 :invalid-key "232"}))
+
+(println "Define schema for the key of a map")
+(def Id-Patient
+  {s/Int Patient})
+(pprint (s/validate Id-Patient {42 {:id 42 :name "RRR"}}))
+; (pprint (s/validate Id-Patient {:id {:id 42 :name "RRR"}})) ; raise error
